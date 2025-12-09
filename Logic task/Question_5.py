f1c06d1433c4b585bcae0a7c2c3a37fa282202e0
muskan9900 <m67359848@gmail.com>
2025-12-09 13:27:43 +0530

    logical task

--- Logic task/Question_5.py
+++ Logic task/Question_5.py
@@ -1,8 +1,38 @@
+
+# started at 11:45 to 1:27
+
 # program to create list of temperature that returns number of days to wait for a warmer temperature 
 
-def temperature_days(days):
-    temperature=[30, 31, 29, 32] # list of temperature 
-    if 
+# list of temperatures 
+
+temperatures=[30,31,29,32]
+def temperature_days(temperatures):
+
+    indices_temp=[] # empty list that stores the indexes of temperature 
+
+    result=[0] * len(temperatures) 
+    # the result list will contain the number of days same as temperature list size 
+
+    for i in range(len(temperatures)):
+
+        while indices_temp and temperatures[i] > temperatures[indices_temp[-1]]:
+            # Calculate days to wait and store in result     
+                indices_temp.append(i)  # Push the current index onto the stack
+
+    return result
+
+
+print(f"the warmer days to wait{temperature_days:} for the temperatures{temperatures}")
+
+    
+
+    
+
+
+    
+
     
     
+    
+
 
