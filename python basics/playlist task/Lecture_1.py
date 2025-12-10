e7b10375022b114615df5d2de0341c8e6f5890e8
muskan9900 <m67359848@gmail.com>
2025-12-10 18:02:50 +0530

    Playlist task question

---
+++ python basics/playlist task/Lecture_1.py
@@ -0,0 +1,38 @@
+# date 10/12/25
+
+"""practice question of lecture 1 VARIABLES AND DATATYPE
+"""
+# question 1 write a program to input 2 numbers  and print  their sum 
+
+num1= int(input("Enter your number one: "))
+
+num2=int(input("Enter your number two: "))
+
+print("the sum of number one and two is:", num1+num2)
+
+
+# question 2 WAP to input  side of a square  and print its area 
+
+side= float(input("enter the side of the sqaure:  "))
+
+print("the area of square equals to: ", side*side)
+
+
+
+# question 3 WAP to input 2 floating numbers  and print their average 
+
+
+num1= float(input("Enter the number 1: "))
+num2=float(input("Enter the number 2:  "))
+
+print("the average of two number is: ", (num1+num2)/2)
+
+
+# question 4 WAP to input 2 integer number a and b , print true if a>=b if not print false
+
+
+a= int(input("enter number a: "))
+
+b= int(input("enter number b: "))
+
+print(a>=b)
---
+++ python basics/playlist task/Lecture_2.py
@@ -0,0 +1,14 @@
+"""practice question of lecture 2 STRINGS AND CONDITIONAL STATEMENTS
+"""
+
+# question 1  WAP to input  user's first name and print its length
+
+str="the $ $ $ $ is in my list"
+
+print("the occurrence of $ in my string", str.count("$"))
+
+
+
+    
+
+
